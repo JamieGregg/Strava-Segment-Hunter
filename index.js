1a5ef49584469992117a59cdbc4d92d51666d5ec
Jamie Gregg <jamiegregg25@gmail.com>
2020-03-26 12:03:01 +0000

    Strava and Env Modules

--- index.js
+++ index.js
@@ -1,6 +1,17 @@
+//Setting up modules
 const express = require('express')
 const bodyParser = require('body-parser')
-const strava = require('strava-v3')
+const stravaAPI = require('strava-v3')
+require('dotenv').config();
+
+
+
+//Strava client
+//var defaultClient = stravaAPI.ApiClient.instance;
+
+// Configure OAuth2 access token for authorization: strava_oauth
+//var strava_oauth = defaultClient.authentications['strava_oauth'];
+//strava_oauth.accessToken = "YOUR ACCESS TOKEN"
 
 const app = express();
 app.use(bodyParser.urlencoded({extended: false}))
@@ -11,4 +22,5 @@ app.get('/', (req, res) => {
 
 app.listen(8000, () => {
   console.log("server is now running")
+  console.log(process.env)
 });
