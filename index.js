5a01eba814ae628ad2f74f3b9f836d4007b63db8
Jamie Gregg <jamiegregg25@gmail.com>
2020-03-26 12:46:35 +0000

    Strava API Connection

--- index.js
+++ index.js
@@ -1,26 +1,22 @@
 //Setting up modules
 const express = require('express')
 const bodyParser = require('body-parser')
-const stravaAPI = require('strava-v3')
 require('dotenv').config();
-
-
-
-//Strava client
-//var defaultClient = stravaAPI.ApiClient.instance;
-
-// Configure OAuth2 access token for authorization: strava_oauth
-//var strava_oauth = defaultClient.authentications['strava_oauth'];
-//strava_oauth.accessToken = "YOUR ACCESS TOKEN"
-
 const app = express();
 app.use(bodyParser.urlencoded({extended: false}))
+var strava = new require("strava")({
+  "access_token"  : process.env.ACCESS_TOKEN,
+  "client_id"     : process.env.CLIENT_ID,
+  "client_secret" : process.env.CLIENT_SECRET,
+  "redirect_url"  : "www.google.com"
+});
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  strava.athlete.get(function(err, res) {
+        console.log(res);
+    });
 });
 
 app.listen(8000, () => {
   console.log("server is now running")
-  console.log(process.env)
 });
