5f160dd931f94d729ec6bef1ff9e935775b4abc5
Jamie Gregg <jamiegregg25@gmail.com>
2020-03-27 10:16:17 +0000

    Refactoring callbacks

--- index.js
+++ index.js
@@ -3,16 +3,17 @@ const express = require('express')
 const bodyParser = require('body-parser')
 require('dotenv').config();
 const app = express();
-app.use(bodyParser.urlencoded({extended: false}))
+app.use(bodyParser.urlencoded({
+  extended: false
+}))
 
 var strava = new require("strava")({
-  "access_token"  : process.env.ACCESS_TOKEN,
-  "client_id"     : process.env.CLIENT_ID,
-  "client_secret" : process.env.CLIENT_SECRET,
-  "redirect_url"  : "www.google.com"
+  "access_token": process.env.ACCESS_TOKEN,
+  "client_id": process.env.CLIENT_ID,
+  "client_secret": process.env.CLIENT_SECRET,
+  "redirect_url": "www.google.com"
 });
 
-var segmentResults = new Array();
 
 //902447 Scarva Drag
 //55274 DCC leaderboard
@@ -20,121 +21,127 @@ var segmentResults = new Array();
 app.use(express.static(__dirname + '/public'));
 
 app.get('/', (req, res) => {
-  res.sendFile('public/index.html', {root: __dirname })
+  res.sendFile('public/index.html', {
+    root: __dirname
+  })
 });
 
 app.listen(8000, () => {
   console.log("server is now running")
-  findDailyRecords(902447, 0, 0)
+  amountOfEfforts(902447, 0)
+
+  //findDailyRecords(902447, 0, 0)
   //findSegmentInfo(902447)
 });
 
 //Find information the person with the key (Me)
-function findAthlete(){
+function findAthlete() {
   strava.athlete.get(function(err, res) {
-        console.log(res);
+    console.log(res);
   });
 }
 
 //CLUB INFORMATION
 
 //Finding information on a club
-function findClub(clubId){
-  strava.clubs.get(clubId, function(err,res){
+function findClub(clubId) {
+  strava.clubs.get(clubId, function(err, res) {
     console.log(res);
   })
 }
 
 //Finding all the members in a club
-function findClubMembers(clubId, totalMembership){
+function findClubMembers(clubId, totalMembership) {
   var params = {
     "id": clubId,
-    "page" : 1,
+    "page": 1,
     "per_page": totalMembership
   }
 
-  strava.clubs.members.get(clubId, params, function(err,data){
+  strava.clubs.members.get(clubId, params, function(err, data) {
     var objJSON = JSON.parse(JSON.stringify(data));
     console.log(objJSON);
   });
 }
 
 //Finds how many people are in a club
-function findClubTotalMembership(clubId){
-  strava.clubs.get(clubId, function(err,res){
-    var objJSON = JSON.parse(JSON.stringify(res.member_count))
-    return objJSON
+function findClubTotalMembership(clubId) {
+  var objJSON = ""
+  strava.clubs.get(clubId, function(err, res) {
+    objJSON = JSON.parse(JSON.stringify(res.member_count))
   })
+  return objJSON
 }
 
 
 //SEGMENT INFORMATION
 
-//Finding the information on any segment
-function findSegmentInfo(segId) {
-  strava.segments.get(segId, function(err,data) {
-    var objJSON = JSON.parse(JSON.stringify(data))
-    var segmentInformation = {
-      "name" : objJSON.name,
-      "distance": convertingMetersToMiles(objJSON.distance),
-      "average_grade": objJSON.average_grade,
-    }
-    console.log(segmentInformation)
+//Returning the anount of times a segment has been done that day.
+function amountOfEfforts(segId, clubId) {
+  var params = {
+    "date_range": "this_year"
+  }
+
+  strava.segments.leaderboard.get(segId, params, function(err, data) {
+    var total = JSON.parse(JSON.stringify(data.effort_count))
+    console.log("Amount of efforts method: " + total)
+    findDailyRecords(segId,total,clubId)
   })
 }
+
 
 //Finding the total results of the day
-function findDailyRecords(segId, segTotal, clubId){
-  var objJSON = [];
-  var segmentResults = [,,,];
-  var amount = segTotal;
-  let total = 0;
-  var input = {
-    "date_range" : "today"
-  }
-  strava.segments.leaderboard.get(segId, input, function(err,data){
-  total = JSON.parse(JSON.stringify(data.effort_count))
-  })
-  if(clubId != 0) {
+function findDailyRecords(segId, segTotal, clubId) {
+
+  if (clubId != 0) {
     var paramsClub = {
-      "date_range" : "today",
-      "per_page" : total,
-      "club_id" : clubId
+      "date_range": "this_year",
+      "per_page": 100,
+      "club_id": clubId
     }
-    strava.segments.leaderboard.get(segId, paramsClub, function(err,data){
-      objJSON = JSON.parse(JSON.stringify(data))
-      return objJSON
+    strava.segments.leaderboard.get(segId, paramsClub, function(err, data) {
+      refineDailyRecords(data)
     })
+
   } else {
     var paramsNoClub = {
-      "date_range" : "today",
-      "per_page" : 100
+      "date_range": "this_year",
+      "per_page": 100
     }
-    strava.segments.leaderboard.get(segId, paramsNoClub, function(err,data){
-      objJSON = JSON.parse(JSON.stringify(data))
-      return objJSON
+
+    strava.segments.leaderboard.get(segId, paramsNoClub, function(err, data) {
+      refineDailyRecords(data)
     })
   }
 }
 
-//Returning the anount of times a segment has been done that day.
-function amountOfEfforts(segId){
-  let total = 0;
-  var params = {
-    "date_range" : "today"
-  }
+function refineDailyRecords(data){
+    var segment = []
+    var numberOfEntry = data.entries.length
 
-  strava.segments.leaderboard.get(segId, params, function(err,data){
-    total = JSON.parse(JSON.stringify(data.effort_count))
-    console.log("Amount of times completed today: " + total)
-    return total;
+    for(let i =0; i < numberOfEntry; i++){
+      segment.push([data.entries[i].athlete_name, data.entries[i].elapsed_time, data.entries[i].rank])
+    }
+
+    console.log(segment)
+}
+
+//Finding the information on any segment
+function findSegmentInfo(segId) {
+  strava.segments.get(segId, function(err, data) {
+    var objJSON = JSON.parse(JSON.stringify(data))
+    var segmentInformation = {
+      "name": objJSON.name,
+      "distance": convertingMetersToMiles(objJSON.distance),
+      "average_grade": objJSON.average_grade }
+    return segmentInformation
   })
 }
 
-function convertingMetersToMiles(meters){
+function convertingMetersToMiles(meters) {
   return (meters * 0.000621371).toFixed(3)
 }
 
-function convertingMilesToKM(miles){
+function convertingMilesToKM(miles) {
   return (miles * 1.60934).toFixed(3)
 }
