7f89defb279cbaa68dd0dc0c937b87709dc4509d
Jamie Gregg <jamiegregg25@gmail.com>
2020-05-03 13:43:52 +0100

    Security update

--- index.js
+++ index.js
@@ -22,10 +22,8 @@ app.use(bodyParser.urlencoded({
 app.enable("trust proxy");
 app.use(function (req, res, next) {
   if (req.secure) {
-    // request was via https, so do no special handling
     next();
   } else {
-    // request was via http, so redirect to https
     res.redirect('https://' + req.headers.host + req.url);
   }
 }); 
